f99181bb13954d54450762f56d4df2d5295c4d77
Ismail IFAKIR <122696795+Ismail-Ifakir@users.noreply.github.com>
2025-04-10 14:48:30 +0100

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
             <TypeAnimation
               data-aos="fade-up"
               sequence={[
-                "Data Scientist | Data Engineer | Data Analyst",
+                "Data Scientist",
                 2000,
 
                 "",
@@ -39,9 +39,9 @@ const Hero = () => {
               // data-aos="fade-up"
               className=" text-[1.1rem] font-medium w-3/4 md:w-full text-gray-600 sm:text-[.95rem]"
             >
-              I am a 24-year-old PhD student passionate about data science, data engineering, and data analytics.
-I’m eager to apply my expertise in these fields, along with machine learning and AI, to impactful projects.
-Driven by curiosity and a commitment to professional growth, I am motivated to contribute to innovative solutions and advance my skills in cutting-edge technologies.
+              I’m a 24-year-old PhD student and aspiring data scientist, with knowledge in data engineering and data analytics.
+I’m eager to apply my expertise in these areas—along with machine learning and AI—to meaningful and impactful projects.
+Fueled by curiosity and a drive for continuous learning, I’m committed to contributing to innovative solutions and mastering cutting-edge technologies.
             </p>
           </div>
           <div data-aos="fade-up" className="buttons flex gap-5">
